28136a209d13ef2e9c9ea54200fa08f1d6831d70
Sanjay Pathak <techiesanjaypathak@idrive.com>
2021-06-03 08:04:20 +0530

    Creating FeedItem.

---
+++ FeedFramework/FeedFramework/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  FeedFramework
+//
+//  Created by SanjayPathak on 03/06/21.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id:UUID
+    let description:String?
+    let location:String?
+    let imageURL:URL
+}
