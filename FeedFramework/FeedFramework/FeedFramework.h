bdd2b10b65f79063ffc39a2b45ba4a315304df0c
Sanjay Pathak <techiesanjaypathak@idrive.com>
2021-06-03 07:56:39 +0530

    Initial setup.

---
+++ FeedFramework/FeedFramework/FeedFramework.h
@@ -0,0 +1,18 @@
+//
+//  FeedFramework.h
+//  FeedFramework
+//
+//  Created by SanjayPathak on 03/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FeedFramework.
+FOUNDATION_EXPORT double FeedFrameworkVersionNumber;
+
+//! Project version string for FeedFramework.
+FOUNDATION_EXPORT const unsigned char FeedFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FeedFramework/PublicHeader.h>
+
+
