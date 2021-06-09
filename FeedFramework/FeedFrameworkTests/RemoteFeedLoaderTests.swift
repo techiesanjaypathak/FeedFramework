0a8432369e6527775db8c9f07687e19348e654c3
Sanjay Pathak <techiesanjaypathak@idrive.com>
2021-06-09 09:50:34 +0530

    Replace by capturing completions in an array, instead of stubbing.

--- FeedFramework/FeedFrameworkTests/RemoteFeedLoaderTests.swift
+++ FeedFramework/FeedFrameworkTests/RemoteFeedLoaderTests.swift
@@ -31,11 +31,10 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_load_deliversErrorOnClientError(){
         let (sut,client) = makeSUT()
-        client.error = NSError(domain: "Test", code: 0, userInfo: nil)
         var capturedErrors = [RemoteFeedLoader.Error]()
-        sut.load {
-            capturedErrors.append($0)
-        }
+        sut.load { capturedErrors.append($0) }
+        let clientError = NSError(domain: "Test", code: 0, userInfo: nil)
+        client.completions[0](clientError)
         XCTAssertEqual(capturedErrors, [.connectivity])
     }
     // MARK:- Helpers
@@ -48,11 +47,9 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     private class HTTPClientSpy:HTTPClient{
         var requestedURLs = [URL]()
-        var error : NSError?
+        var completions = [(Error) -> Void]()
         func get(from url: URL, completion: @escaping (Error) -> Void){
-            if let error  = error {
-                completion(error)
-            }
+            completions.append(completion)
             requestedURLs.append(url)
         }
     }
