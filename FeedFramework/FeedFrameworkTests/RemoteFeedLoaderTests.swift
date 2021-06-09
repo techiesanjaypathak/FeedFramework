4165002d6abff37471b434ccc9f1659774ba6015
Sanjay Pathak <techiesanjaypathak@idrive.com>
2021-06-09 08:04:24 +0530

    adding test so that we can load more than once.

--- FeedFramework/FeedFrameworkTests/RemoteFeedLoaderTests.swift
+++ FeedFramework/FeedFrameworkTests/RemoteFeedLoaderTests.swift
@@ -7,13 +7,6 @@
 
 import XCTest
 
-class HTTPClientSpy:HTTPClient{
-    var requestedURL: URL?
-    func get(from url: URL){
-        requestedURL = url
-    }
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesnotRequestDataFromURL(){
@@ -21,19 +14,35 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL(){
+    func test_load_requestsDataFromURL(){
         let url = URL(string: "https://github.com/techiesanjaypathak/FeedFramework")!
         let (sut,client) = makeSUT(url: url)
         sut.load()
         XCTAssertEqual(client.requestedURL, url)
     }
     
+    func test_load_requestsDataFromURLTwice(){
+        let url = URL(string: "https://github.com/techiesanjaypathak/FeedFramework")!
+        let (sut,client) = makeSUT(url: url)
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestedURLs, [url,url])
+    }
     // MARK:- Helpers
     
     private func makeSUT(url:URL = URL(string: "https://github.com/techiesanjaypathak/FeedFramework")!) -> (RemoteFeedLoader, HTTPClientSpy){
         let client = HTTPClientSpy()
         let sut = RemoteFeedLoader(url: url, client: client)
         return (sut,client)
+    }
+    
+    private class HTTPClientSpy:HTTPClient{
+        var requestedURL: URL?
+        var requestedURLs = [URL]()
+        func get(from url: URL){
+            requestedURL = url
+            requestedURLs.append(url)
+        }
     }
     
 }
