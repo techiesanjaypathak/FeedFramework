2fce2a6c3fa2aeb9138d524ca18ad1c1f2808306
Sanjay Pathak <techiesanjaypathak@idrive.com>
2021-06-12 00:48:57 +0530

    Using custom enum Result type instead of tuple for completion.

--- FeedFramework/FeedFrameworkTests/RemoteFeedLoaderTests.swift
+++ FeedFramework/FeedFrameworkTests/RemoteFeedLoaderTests.swift
@@ -58,19 +58,19 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy:HTTPClient{
-        var messages = [(url:URL, completion:(Error?,HTTPURLResponse?)->Void)]()
+        var messages = [(url:URL, completion:(RemoteFeedResult)->Void)]()
         var requestedURLs : [URL]{
             return messages.map { $0.url }
         }
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void){
+        func get(from url: URL, completion: @escaping (RemoteFeedResult) -> Void){
             messages.append((url,completion))
         }
         func complete(with error:NSError, atIndex index: Int = 0){
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         func complete(withStatusCode code: Int, at index: Int = 0){
-            let httpUrlResponse = HTTPURLResponse(url: messages[index].url, statusCode: code, httpVersion: nil, headerFields: nil)
-            messages[index].completion(nil,httpUrlResponse)
+            let httpUrlResponse = HTTPURLResponse(url: messages[index].url, statusCode: code, httpVersion: nil, headerFields: nil)!
+            messages[index].completion(.success(httpUrlResponse))
         }
     }
     
