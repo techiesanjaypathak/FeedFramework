dc83c9c4b23b2a27a28e7fbf423cac523898d05f
Sanjay Pathak <techiesanjaypathak@idrive.com>
2021-06-12 01:08:46 +0530

    Adding Data to success response and handling invalid JSON on status code 200.

--- FeedFramework/FeedFramework/FeedAPI/RemoteFeedLoader.swift
+++ FeedFramework/FeedFramework/FeedAPI/RemoteFeedLoader.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 public enum RemoteFeedResult{
-    case success(HTTPURLResponse)
+    case success(Data, HTTPURLResponse)
     case failure(Error)
 }
 
--- FeedFramework/FeedFrameworkTests/RemoteFeedLoaderTests.swift
+++ FeedFramework/FeedFrameworkTests/RemoteFeedLoaderTests.swift
@@ -49,6 +49,15 @@ class RemoteFeedLoaderTests: XCTestCase {
         XCTAssertEqual(capturedErrors, Array(repeating: RemoteFeedLoader.Error.invalidData, count: statusCodes.count))
     }
     
+    func test_load_deleversErrorOn200ResponseWithInvalidJSON(){
+        let (sut,client) = makeSUT()
+        var capturedErrors = [RemoteFeedLoader.Error]()
+            sut.load { capturedErrors.append($0) }
+        let invalidJSON = Data(bytes: "invalid json", count: 1)
+        client.complete(withStatusCode: 200, data: invalidJSON,  at: 0)
+        XCTAssertEqual(capturedErrors, [.invalidData])
+    }
+    
     // MARK:- Helpers
     
     private func makeSUT(url:URL = URL(string: "https://github.com/techiesanjaypathak/FeedFramework")!) -> (RemoteFeedLoader, HTTPClientSpy){
@@ -68,9 +77,9 @@ class RemoteFeedLoaderTests: XCTestCase {
         func complete(with error:NSError, atIndex index: Int = 0){
             messages[index].completion(.failure(error))
         }
-        func complete(withStatusCode code: Int, at index: Int = 0){
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0){
             let httpUrlResponse = HTTPURLResponse(url: messages[index].url, statusCode: code, httpVersion: nil, headerFields: nil)!
-            messages[index].completion(.success(httpUrlResponse))
+            messages[index].completion(.success(data, httpUrlResponse))
         }
     }
     
